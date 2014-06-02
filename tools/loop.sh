02e187752b611a9f727d7b2ec93aaf5284c2c5af
Jianqing Peng <pengjianqing@gmail.com>
2014-06-02 22:29:36 +0800

    init

---
+++ tools/loop.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+FILE_LIST=list.txt
+URL="http://api.map.baidu.com/geocoder?address=ADDR&output=json&key=37492c0ee6f924cb5e934fa08c6b1676&city=%E4%B8%8A%E6%B5%B7"
+TMP=address
+mkdir ${TMP} 
+
+while read line
+do
+    echo Get detail for ${line}
+    road=`echo ${line}|cut -d "|" -f2`
+    #echo ${road}
+    get_url=`echo ${URL}|sed "s/ADDR/${road}/g"`
+    filename=`echo ${road}|md5sum|cut -d " " -f1`
+    wget  ${get_url} -O ${TMP}/${filename}_${road}.json
+done<${FILE_LIST}
